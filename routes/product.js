a7ae825e0ffaa56689ab25011906cb136155668c
Branislav Carkic <branislavcharkic@gmail.com>
2023-07-27 22:48:19 +0200

    fixed some typo

--- routes/product.js
+++ routes/product.js
@@ -1,33 +1,33 @@
 const Product = require('../models/product');
-const express = reqire('express');
+const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const productsList = await Product.find();
-  
-    if(!productsList) {
-      res.status(500).json({success: false});
-    }
-  
-    res.send(productsList);
-  
-  });
-  
-  router.post(`/`, (req, res) => {
-    const product = new Product({
-      name: req.body.name,
-      image: req.body.image,
-      price: req.body.price,
-    });
-  
-    product
-      .save()
-      .then((createdProduct) => {
-        res.status(200).json(createdProduct);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err, success: false });
-      });
-    });
+  const productsList = await Product.find();
 
-  module.exports = router;
+  if (!productsList) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(productsList);
+
+});
+
+router.post(`/`, (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    price: req.body.price,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(200).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err, success: false });
+    });
+});
+
+module.exports = router;
