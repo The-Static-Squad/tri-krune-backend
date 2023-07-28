56d6f16f23c7837174c07135f548ecc010900216
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-07-28 23:51:23 +0200

    Controllers created (get all, get by id, post)

--- routes/product.js
+++ routes/product.js
@@ -1,33 +1,16 @@
-const Product = require('../models/product');
+const {
+	getAllProducts,
+	getProductById,
+	addProduct
+} = require('../controllers/productControllers');
+
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const productsList = await Product.find();
+router.get('/', getAllProducts);
 
-  if (!productsList) {
-    res.status(500).json({ success: false });
-  }
+router.get('/:id', getProductById)
 
-  res.send(productsList);
-
-});
-
-router.post(`/`, (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    price: req.body.price,
-  });
-
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(200).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err, success: false });
-    });
-});
+router.post('/', addProduct)
 
 module.exports = router;
