01190a9ef8536fc8805deb72bf1f94b64849916b
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-08-07 00:08:12 +0200

    Product model made more detailed, timestamps and requred fields added

--- models/product.js
+++ models/product.js
@@ -1,9 +1,39 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    price: Number
-  });
+const productSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true
+		},
+		category: {
+			type: String,
+			required: true
+		},
+		description: {
+			type: String,
+			required: false
+		},
+		images: {
+			type: [String],
+			required: true
+		},
+		price: {
+			type: Number,
+			required: true
+		},
+		discountPrice: {
+			type: Number,
+			required: false
+		},
+		tags: {
+			type: [String],
+			required: false
+		}
+	},
+	{
+		timestamps: true
+	}
+);
 
 module.exports = mongoose.model("Product", productSchema);
