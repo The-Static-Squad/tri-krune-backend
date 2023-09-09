7cd33ed4ff74f88afbcde8dbc5f25afcc4699d7f
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-09-09 12:13:48 +0200

    unnecessary deleted

--- __Back/README.txt
+++
@@ -1,1 +0,0 @@
-Tri Krune backend
--- __Back/routes/search.js
+++
@@ -1,9 +0,0 @@
-const { request } = require("express");
-
-const express = require('express');
-const { searchByText } = require("../controllers/searchControllers");
-const router = express.Router();
-
-router.get('/', searchByText);
-
-module.exports = router;
