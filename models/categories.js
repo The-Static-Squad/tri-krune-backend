80f9dfb7ca9234bc3a67a2d918588b36413c4c46
Slou A <slowautomobile@protonmail.com>
2023-09-14 13:43:02 +0200

    added icon property to category

--- models/categories.js
+++ models/categories.js
@@ -5,6 +5,10 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  icon: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("categories", categorySchema);
