39e702e79a94e0708fb009d649da927ef6845a55
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-10-18 13:45:03 +0200

    Update product method-version 2 (bugs fixed) Add product method bugs fixed

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const { findById } = require('../models/categories');
 
+//Method to convert request strins that hold values for array-fields
+//splits tag string on each , or whitespace (single or sequence)
 const toArray = (str) => {
 	return str.split(/[,\s]+/).filter(elem => elem);
 }
@@ -41,28 +43,18 @@ const addProduct = async (req, res) => {
 
 	//convert tags input from a string to an array
 	if (product.tags.length !== 0) {
-		//split tag string on each , or whitespace (single or sequence)
-		const tagArray = product.tags[0].split(/[,\s]+/);
-		//ensure to exclude last empty string element, if tag string ends with , or whitespace
-		tagArray.filter(tag => tag);
-		product.tags = tagArray;
+		product.tags = toArray(product.tags);
 	}
 
 	//add image paths to appropriate fields
-	const images = req.files;
-	if (images.length > 0) {
-		const mainImg = images['prodImage'][0];
-		const additionalImgs = images['addImages']
+	if (req.files.prodImage) {
+		product.mainImg = req.files.prodImage[0].path;
+	}
 
-		if (mainImg) {
-			product.mainImg = mainImg.path;
-		}
-
-		if (additionalImgs) {
-			additionalImgs.forEach(image => {
-				product.pictures.push(image.path);
-			});
-		}
+	if (req.files.addImages) {
+		req.files.addImages.forEach(image => {
+			product.pictures.push(image.path);
+		});
 	}
 
 	try {
@@ -99,7 +91,7 @@ const deleteProduct = async (req, res) => {
 	});
 
 	if (deleteErrors.length > 0) {
-		return res.status(207).json({ message: "Product deleted, with errors in deleting files", images_not_deleted: deleteErrors, deleted_product: deletedProduct, })
+		return res.status(207).json({ message: "Product deleted, with errors in deleting files", images_not_deleted: deleteErrors, deleted_product: deletedProduct })
 	}
 
 	res.status(200).json(deletedProduct);
@@ -109,7 +101,7 @@ const updateProduct = async (req, res) => {
 	const id = req.params.id;
 	const newValues = req.body;
 
-	//Chceck if id passed as request parameter is valid mongodb id
+	//Check if id passed as request parameter is valid mongodb id
 	if (!mongoose.Types.ObjectId.isValid(id)) {
 		return res(400).json({ message: 'invalid Id' });
 	}
@@ -120,72 +112,25 @@ const updateProduct = async (req, res) => {
 	}
 
 	const product = await Product.findById(id);
+
+	if (!product) {
+		return res.status(404).json({ message: 'Product hasn\'t been found' });
+	}
+
 	let oldMainImage = product.mainImg; //previosly set main image
 	let oldAddImages = product.pictures; //previosly set additional images
 
 	const deleteErrors = [];
 
-	//Check if new images had been added to request
-	//Find and delete from the base previosly attached images
-	//Add new image paths to values to be updated
-	if (req.files.length > 0) {
-		//The first value in the first array responds to the product main image
-		//(Input field prodImage provides an array of attached files; it is limited to one value)
-		const newMainImg = req.files.prodImage[0];
-		if (newMainImg) {
 
-			//remove old main image from the base, if set
-			if (oldMainImage) {
-				try {
-					fs.unlinkSync(oldMainImage);
-				} catch (err) {
-					deleteErrors.push(err);
-				}
-			}
+	//Handle main image update
+	//************************
+	//Check if the request holds the path of main image that should be kept
+	if (!newValues.mainImg) {
 
-			//Add path of new image to update values
-			newValues.mainImg = newMainImg.path;
-		}
-
-		//The values in the second array respond to the product additional images
-		//(Input field addImages provides an array of attached files; it is limited to max three values)
-		const newAdditionalImages = req.files.addImages;
-
-		if (newAdditionalImages.length > 0) {
-
-			//Identify old images to be removed-
-			//Compare images' paths to be kept, sent by request body, with all previosly attached images' paths
-			//String of pictures to be kept recieved in req is converted to array
-			if (!newValues.pictures) {
-				//If there's no pictures to be saved, create an empty array
-				newValues.pictures = [];
-			} else {
-				if (typeof newValues.pictures === "string") {
-					newValues.pictures = newValues.pictures.split(',');
-				}
-			}
-
-			//Compare arrays of images already attached to previous product version and delete unnecessary
-			oldAddImages.forEach(oldImg => {
-				if (!newValues.pictures.includes(oldImg)) {
-					try {
-						fs.unlinkSync(oldImg);
-						console.log(oldImg + " deleted");
-					} catch (err) {
-						deleteErrors.push(err);
-					}
-				}
-			});
-
-			//Add paths of new images to update values
-			newValues.pictures = newValues.pictures.concat(newAdditionalImages.map(img => img.path));
-		}
-	} else {
-		//If no new images have been attached, and there are no images to keep in request
-		//Delete old images from the base
-
-		if (oldMainImage && !newValues.mainImg) {
-			console.log(oldMainImage, newValues.mainImg)
+		console.log("delete main")
+		//Remove old main image from the base, if set
+		if (oldMainImage) {
 			try {
 				fs.unlinkSync(oldMainImage);
 			} catch (err) {
@@ -193,28 +138,68 @@ const updateProduct = async (req, res) => {
 			}
 		}
 
-		if (oldAddImages.length > 0 && newValues.pictures.length===0) {
-			console.log(oldAddImages, newValues.pictures)
-			oldAddImages.forEach(oldImg => {
-				try {
-					fs.unlinkSync(oldImg);
-				} catch (err) {
-					deleteErrors.push(err);
-				}
-			});
+		//Check if new image had been added to request
+		if (req.files.prodImage) {
+			//Add new image path to the update values
+			//The first value in the first array responds to the product main image
+			//(Input field prodImage provides an array of attached files; it is limited to one value)
+			//Attach new main image path
+			newValues.mainImg = req.files.prodImage[0].path;
+		} else {
+			//remove main image from the product
+			newValues.mainImg = '';
 		}
+	}
+
+	//Handle additional images update
+	//*******************************
+
+	//Check if the request holds the paths of images that should be kept
+	//Make sure to get an array
+	if (!newValues.pictures) {
+		//If there's no pictures to be saved, create an empty array
+		newValues.pictures = [];
+	} else {
+		//If request holds a string of paths, convert it to array
+		if (typeof newValues.pictures === "string") {
+			newValues.pictures = newValues.pictures.split(',');
+		}
+	}
+
+	//Compare images' paths to be kept, sent by request body, with all previosly attached images' paths
+	//Compare arrays of images already attached to previous product version and delete unnecessary
+	oldAddImages.forEach(oldImg => {
+		if (!newValues.pictures.includes(oldImg)) {
+			try {
+				fs.unlinkSync(oldImg);
+			} catch (err) {
+				deleteErrors.push(err);
+			}
+		}
+	});
+
+	//Check if new image(s) had been added to request
+	//Add new image path(s) to the update values
+	//The values in the second array respond to the product additional images
+	//(Input field addImages provides an array of attached files; it is limited to max three values)
+	if (req.files.addImages) {
+		//Add paths of new images to update values
+		newValues.pictures = newValues.pictures.concat(req.files.addImages.map(img => img.path));
 	}
 
 	let productToUpdate = await Product.findOneAndUpdate({ _id: id }, { ...newValues });
 
-	if (!productToUpdate) {
-		return res.status(404).json({ message: 'Product hasn\'t been found', newVals: newValues.tags });
+	if (deleteErrors.length > 0) {
+		return res.status(207).json({
+			message: "Product deleted, with errors in deleting files",
+			images_not_deleted: deleteErrors,
+			newValues,
+			productToUpdate
+		})
 	}
 
 	res.status(200).json({ productToUpdate, newValues });
-
 }
-
 
 module.exports = {
 	getAllProducts,
